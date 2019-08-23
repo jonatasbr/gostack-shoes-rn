c53e3d3ab2eb2b87c5a3f01da6a28e7bfb34a18d
Jonatas Braz <jonatasbraz@gmail.com>
2019-08-23 00:57:57 -0300

    style of the cart and his redux

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,19 +1,87 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Products,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+  EmptyContainer,
+  EmptyText,
+} from './styles';
 
-const styles = StyleSheet.create({
-  testText: {
-    color: '#000',
-    fontSize: 20,
-  },
-});
-
-export default function Cart() {
+function Cart({ products }) {
   return (
-    <View>
-      <Text style={styles.testText}>Cart</Text>
-    </View>
+    <Container>
+      {products.length ? (
+        <>
+          <Products>
+            {products.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{ uri: product.image }} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{product.priceFormatted}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete>
+                    <Icon name="delete-forever" size={24} color="#7459c1" />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color="#7459c1"
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={String(product.amount)} />
+                  <ProductControlButton>
+                    <Icon name="add-circle-outline" size={20} color="#7459c1" />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+          </Products>
+          <TotalContainer>
+            <TotalText>TOTAL</TotalText>
+            <TotalAmount>R$ reais</TotalAmount>
+            <Order>
+              <OrderText>FINALIZAR PEDIDO</OrderText>
+            </Order>
+          </TotalContainer>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Icon name="remove-shopping-cart" size={64} color="#eee" />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
+    </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  products: state.cart.map(product => ({
+    ...product,
+  })),
+});
+
+export default connect(mapStateToProps)(Cart);
