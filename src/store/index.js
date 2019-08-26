f78f844ed40c0815d3254f48b25f4f53eb672b1b
Jonatas Braz <jonatasbraz@gmail.com>
2019-08-26 01:14:43 -0300

    reactotron-redux-saga - with SagaMonitor

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,8 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-// const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
-// const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const enhancer = __DEV__
   ? compose(
